b3c57eefbace5e384908695418d635566f0d98ff
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-12-02 18:33:24 +0100

    place buildings

---
+++ BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField] private Transform pfWoodHarvester;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+    }
+
+}
