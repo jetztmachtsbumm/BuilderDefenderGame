8f004a498184ecce826e41059ab6213791dc9e14
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-12-08 13:33:54 +0100

    Resource Nodes

--- BuilderDefenderGame/Assets/Scripts/ResourceGenerator.cs
+++ BuilderDefenderGame/Assets/Scripts/ResourceGenerator.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class ResourceGenerator : MonoBehaviour
 {
 
-    private BuildingTypeSO buildingType;
+    private ResourceGeneratorData resourceGeneratorData;
     private float timer;
     private float timerMax;
+    private int nearbyResourceAmount = 0;
 
     private void Awake()
     {
-        buildingType = GetComponent<BuildingTypeHolder>().buildingType;
-        timerMax = buildingType.resourceGeneratorData.timerMax;
+        resourceGeneratorData = GetComponent<BuildingTypeHolder>().buildingType.resourceGeneratorData;
+        timerMax = resourceGeneratorData.timerMax;
+    }
+
+    private void Start()
+    {
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, resourceGeneratorData.resourceDetectionRadius);
+
+        foreach(Collider2D collider in collider2DArray)
+        {
+            ResourceNode resourceNode = collider.GetComponent<ResourceNode>();
+            if(resourceNode != null)
+            {
+                if(resourceNode.resourceType == resourceGeneratorData.resourceType)
+                {
+                    nearbyResourceAmount++;
+                }
+            }
+        }
+
+        nearbyResourceAmount = Mathf.Clamp(nearbyResourceAmount, 0, resourceGeneratorData.maxResourceAmount);
+
+        if(nearbyResourceAmount == 0)
+        {
+            enabled = false;
+        }
     }
 
     private void Update()
@@ -21,7 +46,7 @@ public class ResourceGenerator : MonoBehaviour
         if(timer <= 0f)
         {
             timer += timerMax;
-            ResourceManager.Instance.AddResource(buildingType.resourceGeneratorData.resourceType, 1);
+            ResourceManager.Instance.AddResource(resourceGeneratorData.resourceType, nearbyResourceAmount);
         }
     }
 
