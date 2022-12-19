7f01f7e05769c2093f48152f454a262c4f4364cb
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-12-20 00:11:39 +0100

    BuildingConstruction

--- BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
+++ BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
@@ -45,7 +45,8 @@ public class BuildingManager : MonoBehaviour
                     if (ResourceManager.Instance.CanAfford(activeBuildingType.constructionResourceCostArray))
                     {
                         ResourceManager.Instance.SpendResources(activeBuildingType.constructionResourceCostArray);
-                        Instantiate(activeBuildingType.prefab, UtilsClass.GetMouseWorldPosition(), Quaternion.identity);
+                        //Instantiate(activeBuildingType.prefab, UtilsClass.GetMouseWorldPosition(), Quaternion.identity);
+                        BuildingConstruction.Create(UtilsClass.GetMouseWorldPosition(), activeBuildingType);
                     }
                     else
                     {
